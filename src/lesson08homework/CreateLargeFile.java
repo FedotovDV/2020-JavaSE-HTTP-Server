8e40190ed8dcfd7a98360a007ef77cbd5dccc699
Dmitriy <fed3375546@yandex.ru>
2020-03-24 01:17:46 +0300

    @author devstudy

---
+++ src/lesson08homework/CreateLargeFile.java
@@ -0,0 +1,28 @@
+package lesson08homework;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * 
+ * @author devstudy
+ * @see http://devstudy.net
+ */
+public class CreateLargeFile {
+
+	public static void main(String[] args) throws IOException {
+		String s = "Java is a general-purpose computer programming language that is concurrent, class-based, object-oriented, and specifically designed to have as few implementation dependencies as possible.";
+		try (Writer wr = new BufferedWriter(new FileWriter("large.txt"))) {
+			for (int i = 0; i < 20000000; i++) {
+				wr.write(s + "\r\n");
+			}
+			wr.flush();
+		}
+
+		System.out.println("File created: " + new File("large.txt").length() + " bytes");
+	}
+
+}
---
+++ src/lesson08homework/IOUtils.java
@@ -0,0 +1,35 @@
+package lesson08homework;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Path;
+import java.util.List;
+
+/**
+ * 
+ * @author devstudy
+ * @see http://devstudy.net
+ */
+public interface IOUtils {
+	/*
+	Копирование стороки из входного потока в выходной, попутно заменяя символы из
+	которые есть в inChars, на символы которые находятся в той же позиции в outChars.
+	Строки inChars и outChars должны быть одинаковой длинны. Если inChars или outChars ==
+	null считаем что на вход была подана пустая строка. Пример: in = 1234567890 inChars =
+	357 outChars = 480 out = 1244860890. Потоки после работы закрыть!
+	NullPointerException - если любой из параметров in или out == null
+	IllegalArgumentException - если inChars.length != outChars.length
+	IOException - если при работе с потоками произошла ошибка
+	*/
+	void replaceChars(Reader in, Writer out, String inChars, String outChars)
+			throws NullPointerException, IllegalArgumentException, IOException;
+	/*Ищет файлы в которых есть вхождение строки textToFind с учетом регистра ignoreCase=false
+	или без регистра ignoreCase=true
+	NullPointerException - если любой из параметров rootDir или textToFind == null.
+	IllegalArgumentException - если rootDir - не директория.
+	IOException - если При работе с потоками произошла ошибка
+	*/
+	List<Path> findText(Path rootDir, String textToFind, boolean ignoreCase)
+			throws NullPointerException, IllegalArgumentException, IOException;
+}
