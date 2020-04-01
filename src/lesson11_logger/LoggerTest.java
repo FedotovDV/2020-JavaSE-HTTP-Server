8aa64310e571cd27ca2961de081b4134a770efb8
Dmitriy <fed3375546@yandex.ru>
2020-04-02 00:05:42 +0300

    lesson 11 logger practice

---
+++ src/lesson11_logger/LoggerTest.java
@@ -0,0 +1,12 @@
+package lesson11_logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerTest {
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoggerTest.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("Hello world");
+	}
+}
